abe538ab7f23da026aea85a3054a7dc08c71afdd
tom.shlomo <tom.shlomo@ridewithvia.com>
2024-03-16 07:00:38 +0200

    template renames

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './json_unix_json.js',
+  './json_unix_json_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
